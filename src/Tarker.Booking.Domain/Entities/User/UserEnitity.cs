33c770de9f25ff38ab2877f4b7f445d1d0b524a1
jhaycodee <kluis0178@gmaill.com>
2024-07-11 20:55:52 -0600

    Add project files.

---
+++ src/Tarker.Booking.Domain/Entities/User/UserEnitity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarker.Booking.Domain.Entities.User
+{
+    public class UserEnitity
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password{ get; set; }
+    }
+}
