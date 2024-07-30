cb5bca851a73498d323f8fc9323fb5e3a73d29b3
jhaycodee <kluis0178@gmaill.com>
2024-07-29 21:40:19 -0600

    creacion de controladors  api

---
+++ src/Tarker.Booking.Api/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarker.Booking.Api.Controllers
+{
+    [Route("api/v1/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            
+        }
+    }
+}
--- src/Tarker.Booking.Api/Program.cs
+++ src/Tarker.Booking.Api/Program.cs
@@ -1,10 +1,8 @@
 using Tarker.Booking.Api;
-using Tarker.Booking.Common;
 using Tarker.Booking.Application;
+using Tarker.Booking.Common;
 using Tarker.Booking.External;
 using Tarker.Booking.Persistence;
-using Tarker.Booking.Application.DataBase.User.Commands.CreateUser;
-using Tarker.Booking.Application.DataBase.User.Commands.UpdateUserModel;
 
 // Creamos una instancia del constructor de la aplicación, que configura y construye la aplicación web.
 var builder = WebApplication.CreateBuilder(args);
@@ -17,23 +15,9 @@ builder.Services
     .AddExternal(builder.Configuration)
     .AddPersistence(builder.Configuration);
 
+builder.Services.AddControllers();
 
 // Construimos la aplicación, preparando todos los servicios y configuraciones necesarios.
 var app = builder.Build();
-
-app.MapPost("/testService", async (IUpdateUserCommand service) =>
-{
-    var model = new UpdateUserModel
-    {
-        UserId = 2,
-        FirstName = "Jodue 2",
-        LastName = "alala 2",
-        UserName = "wqds2",
-        Password = "01234",
-    };
-
-    return await service.Execute(model);
-});
-
-// Configuramos el pipeline de solicitudes HTTP y comenzamos a escuchar solicitudes entrantes.
+app.MapControllers();
 app.Run();
